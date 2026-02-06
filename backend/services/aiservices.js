acb26ce610772ce149486e7f10c2103dd22bb252
talhadevstree <talha.s@devstree.in>
2026-02-06 11:30:36 +0530

    changemodel

--- backend/services/aiservices.js
+++ backend/services/aiservices.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const genAI = new GoogleGenerativeAI(process.env.API);
 const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-2.5-flash",
     systemInstruction: `
                 Here’s a solid system instruction for your AI code reviewer:
 
